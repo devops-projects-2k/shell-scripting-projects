a723f4521395fa449c6c922cc70e3b7fbb8bb819
muthu <msg.rmuthu@gmail.com>
2024-12-14 13:40:31 +0000

    list ofcollaborators script added

---
+++ github-api/list-collaborators.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+#######################
+# Name: list-collaborators.sh
+# Version: v1
+# Description: 
+# Lists collaborators present in github project
+#
+#
+#
+#
+# Date Created: 12/14/2024
+################################################
+
+echo "List of collaborators"
